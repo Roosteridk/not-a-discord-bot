781cee0c29bae9213ed0212a7c01df3be6526f13
Arda <roosterdogan@gmail.com>
2022-12-17 22:40:34 -0500

    added parameter

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import Discord from "./mod.ts";
 const bot = new Discord(
   Deno.env.get("DISCORD_TOKEN")!,
   Deno.env.get("DISCORD_PUBLIC_KEY")!,
+  BigInt(Deno.env.get("DISCORD_APPLICATION_ID")!),
 );
 
 Deno.serve((req) => {
